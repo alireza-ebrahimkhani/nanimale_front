95a4cb3fae1295b8b423d2d45c123668aca992e6
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2022-07-11 20:02:06 +0430

    a

--- js/js.js
+++ js/js.js
@@ -1,10 +1,28 @@
-const hamico = document.querySelector(".header-hamico")
-const line = document.querySelector(".header-line")
-const drawer = document.querySelector(".header-drawer")
-const body = document.querySelector("body")
+let hamico = document.querySelector(".header-hamico")
+let line = document.querySelector(".header-line")
+let drawer = document.querySelector(".header-drawer")
+let body = document.querySelector("body")
 
 hamico.addEventListener('click' , ()=>{
     line.classList.toggle("header-line--open")
     drawer.classList.toggle("header-drawer--open")
     body.classList.toggle("freeze")
 })
+
+// fixed footer
+let main = document.querySelector(".main")
+let footer = document.querySelector(".footer")
+let footer_height = footer.clientHeight;
+if (screen.width > 700) {
+    main.style.marginBottom = footer_height + "px"
+}
+window.addEventListener('resize' , ()=>{
+    footer_height = footer.clientHeight;
+    if (screen.width > 700) {
+        main.style.marginBottom = footer_height + "px"
+    }else{
+        main.style.marginBottom = 0 + "px"
+    }
+    
+})
+
