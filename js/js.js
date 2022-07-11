0c21bf2be4cf21321035e1b9da70ab98c3174574
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2022-07-11 13:27:07 +0430

    a

--- js/js.js
+++ js/js.js
@@ -1,8 +1,10 @@
 const hamico = document.querySelector(".header-hamico")
 const line = document.querySelector(".header-line")
 const drawer = document.querySelector(".header-drawer")
+const body = document.querySelector("body")
 
 hamico.addEventListener('click' , ()=>{
     line.classList.toggle("header-line--open")
     drawer.classList.toggle("header-drawer--open")
+    body.classList.toggle("freeze")
 })
