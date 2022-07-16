b130c90b3190c46182ce523dd879cb00f0081b73
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2022-07-16 18:37:18 +0430

    a

--- js/js.js
+++ js/js.js
@@ -13,15 +13,44 @@ hamico.addEventListener('click' , ()=>{
 let main = document.querySelector(".main")
 let footer = document.querySelector(".footer")
 let footer_height = footer.clientHeight;
-if (screen.width > 700) {
+if (window.innerWidth >= 700) {
     main.style.marginBottom = footer_height + "px"
+}else {
+    main.style.marginBottom = 0 ;
 }
 window.addEventListener('resize' , ()=>{
     footer_height = footer.clientHeight;
-    if (screen.width > 700) {
+    if (window.innerWidth >= 700) {
+        console.log('hi')
         main.style.marginBottom = footer_height + "px"
     }else{
         main.style.marginBottom = 0 + "px"
     }
-    
+})
+
+// rotate find me 
+let star = document.querySelector('.star-shape')
+window.onscroll = function () {
+    scrollRotate();
+};
+function scrollRotate() {
+    star.style.transform = "rotate(" + window.pageYOffset/8 + "deg)";
+}
+
+ //hide find me
+ let star_btn = document.querySelector('.star')
+window.addEventListener('scroll' , ()=>{
+    if (window.innerWidth >= 700) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                star_btn.classList.add("star--hide");
+            }else{
+                star_btn.classList.remove("star--hide");
+            }
+    }else{
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - footer.clientHeight) {
+            star_btn.classList.add("star--hide");
+        }else{
+            star_btn.classList.remove("star--hide");
+        }
+    }
 })
